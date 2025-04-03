8784fc06deb939419b47baa01e2feaa5b40dfdd2
Harry-Ell <h.ellingham@lancaster.ac.uk>
2025-04-03 10:16:39 +0100

    fields outputted

--- GP_Implementation/Kernel.py
+++ GP_Implementation/Kernel.py
@@ -49,6 +49,8 @@ def sample_vector_field(grid_x: np.ndarray, grid_y: np.ndarray, grid_z: np.ndarr
     """
 
     np.random.seed(random_seed)
+    # some hacky logic 
+    function_chosen = divergence_free_kernel if kernel_func == 'divergence_free_kernel' else divergence_and_curl_free_kernel
 
     points = np.vstack([grid_x.flatten(), grid_y.flatten(), grid_z.flatten()]).T
     N = points.shape[0]  # total number of spatial points
@@ -56,16 +58,11 @@ def sample_vector_field(grid_x: np.ndarray, grid_y: np.ndarray, grid_z: np.ndarr
 
     # Build the full covariance matrix (3N x 3N)
     cov = np.zeros((N * D, N * D))
-    if kernel_func == 'divergence_free_kernel':
-        for i in range(N):
-            for j in range(N):
-                K_ij = divergence_free_kernel(points[i], points[j], sigma_f=sigma_f, l=l)  # (3,3)
-                cov[i*D:(i+1)*D, j*D:(j+1)*D] = K_ij
-    else:
-        for i in range(N):
-            for j in range(N):
-                K_ij = divergence_and_curl_free_kernel(points[i], points[j], sigma_f=sigma_f, l=l)  # (3,3)
-                cov[i*D:(i+1)*D, j*D:(j+1)*D] = K_ij
+
+    for i in range(N):
+        for j in range(N):
+            K_ij = function_chosen(points[i], points[j], sigma_f=sigma_f, l=l)  # (3,3)
+            cov[i*D:(i+1)*D, j*D:(j+1)*D] = K_ij
 
     # generate a sample from a mean 0 field
     mean = np.zeros(N * D)
@@ -83,43 +80,66 @@ def sample_vector_field(grid_x: np.ndarray, grid_y: np.ndarray, grid_z: np.ndarr
 def updated_vector_field(sampled_points_x: np.ndarray, sampled_points_y: np.ndarray, sampled_points_z: np.ndarray,
                          sampled_fields_x: np.ndarray, sampled_fields_y: np.ndarray, sampled_fields_z: np.ndarray,
                          grid_x: np.ndarray, grid_y: np.ndarray, grid_z: np.ndarray,
-                         kernel_func:str, sigma_f: float = 1, l: float = 1, random_seed: int = 1):
+                         kernel_func: str, sigma_f: float = 1, l: float = 1, random_seed: int = 1):
     """
-    Similar to before except we are updating given measurements at some points. Not sure if this is 
-    the best way to do this at this point, so we will see if this starts to go pear shaped. 
-
-    The first 6 args are the sampled points at which we choose to take the fields. 
-    The remaining 6 args are all of the grids to evaluate at, and some extra things like what 
-    kernel function you want
+    Compute the GP-posterior vector field given noisy vector observations at a few points.
+    returns a 3D vector field evaluated over the entire grid, conditioned on known field measurements.
     """
 
     np.random.seed(random_seed)
 
-    points = np.vstack([grid_x.flatten(), grid_y.flatten(), grid_z.flatten()]).T
-    N = points.shape[0]  # total number of spatial points
-    D = 3  # dimensionality
+    # Choose kernel
+    function_chosen = divergence_free_kernel if kernel_func == 'divergence_free_kernel' else divergence_and_curl_free_kernel
 
-    # Build the full covariance matrix (3N x 3N)
-    cov = np.zeros((N * D, N * D))
-    if kernel_func == 'divergence_free_kernel':
-        for i in range(N):
-            for j in range(N):
-                K_ij = divergence_free_kernel(points[i], points[j], sigma_f=sigma_f, l=l)  # (3,3)
-                cov[i*D:(i+1)*D, j*D:(j+1)*D] = K_ij
-    else:
-        for i in range(N):
-            for j in range(N):
-                K_ij = divergence_and_curl_free_kernel(points[i], points[j], sigma_f=sigma_f, l=l)  # (3,3)
-                cov[i*D:(i+1)*D, j*D:(j+1)*D] = K_ij
+    # Stack and flatten inputs
+    points = np.vstack([grid_x.flatten(), grid_y.flatten(), grid_z.flatten()]).T               # (N, 3)
+    points_sampled = np.vstack([sampled_points_x.flatten(), sampled_points_y.flatten(), sampled_points_z.flatten()]).T  # (M, 3)
+    fields_sampled = np.vstack([sampled_fields_x.flatten(), sampled_fields_y.flatten(), sampled_fields_z.flatten()]).T  # (M, 3)
 
-    # generate a sample from a mean 0 field
-    mean = np.zeros(N * D)
-    sample = np.random.multivariate_normal(mean, cov)
+    N = points.shape[0]           # number of grid points
+    M = points_sampled.shape[0]   # number of observations
+    D = 3                         # vector field dimensionality
 
-    # reshape for plotting purposes    
-    field_vectors = sample.reshape(N, D)   
+    # Allocate covariance matrices
+    K_x_x = np.zeros((M * D, M * D))        # covariance between sampled points
+    K_xstar_x = np.zeros((N * D, M * D))    # covariance between grid and sampled
+    K_xstar_xstar = np.zeros((N * D, N * D))  # covariance between grid points
+
+    # Build K_x_x
+    for i in range(M):
+        for j in range(M):
+            K_ij = function_chosen(points_sampled[i], points_sampled[j], sigma_f=sigma_f, l=l)  # (3x3)
+            K_x_x[i*D:(i+1)*D, j*D:(j+1)*D] = K_ij
+
+    # Build K_xstar_x
+    for i in range(N):
+        for j in range(M):
+            K_ij = function_chosen(points[i], points_sampled[j], sigma_f=sigma_f, l=l)  # (3x3)
+            K_xstar_x[i*D:(i+1)*D, j*D:(j+1)*D] = K_ij
+
+    # Build K_xstar_xstar
+    for i in range(N):
+        for j in range(N):
+            K_ij = function_chosen(points[i], points[j], sigma_f=sigma_f, l=l)  # (3x3)
+            K_xstar_xstar[i*D:(i+1)*D, j*D:(j+1)*D] = K_ij
+
+    # Flatten observations into single vector y
+    y = np.zeros(M * D)
+    for i in range(M):
+        y[i*D:(i+1)*D] = fields_sampled[i]  # (3x,)
+
+    # GP posterior mean and covariance
+    K_inv = np.linalg.inv(K_x_x)
+    posterior_mean = K_xstar_x @ K_inv @ y
+    posterior_covar = K_xstar_xstar - K_xstar_x @ K_inv @ K_xstar_x.T
+
+    # Sample from posterior
+    sample = np.random.multivariate_normal(posterior_mean, posterior_covar)
+
+    # Reshape into 3D vector field
+    field_vectors = sample.reshape(N, D)
     U = field_vectors[:, 0].reshape(grid_x.shape)
     V = field_vectors[:, 1].reshape(grid_y.shape)
     W = field_vectors[:, 2].reshape(grid_z.shape)
-    
+
     return grid_x, grid_y, grid_z, U, V, W
